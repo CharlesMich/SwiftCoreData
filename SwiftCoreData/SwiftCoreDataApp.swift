ad67c52a99c74de9347e85bb80da796ac0b3c16b
CharlesMich <charleedongo@hotmail.com>
2025-04-02 23:31:55 -0500

    Initial Commit

---
+++ SwiftCoreData/SwiftCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoreDataApp.swift
+//  SwiftCoreData
+//
+//  Created by Charles Michael on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
